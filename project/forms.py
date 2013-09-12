bfce75313bf24b7d21056c04948304acebb40bce
root <root@pegasus.(none)>
2013-09-11 21:18:42 -0400

    add model images to accordion

--- project/forms.py
+++ project/forms.py
@@ -3,18 +3,14 @@ from django.forms import ModelForm
 from project.models import Project
 	
 class UploadDraftForm(forms.Form):
-	#stlname = forms.CharField(max_length=50)
+
 	stl = forms.FileField(required=False)
-	
-	#dxfname = forms.CharField(max_length=50)
 	dxf = forms.FileField(required=False)
 	
 class UploadModelForm(forms.Form):
-	#topname = forms.CharField(max_length=50)
+
 	topView = forms.ImageField(required=False)
-	
-	#rightname = forms.CharField(max_length=50)
-	rightView = forms.ImageField(required=False)
-	
-	#frontname = forms.CharField(max_length=50)
+	View34 = forms.ImageField(required=False)
+	leftView = forms.ImageField(required=False)
 	frontView = forms.ImageField(required=False)
+	rightView = forms.ImageField(required=False)
